885d29dc6bd9941fd9856edfdfe8c727909134c6
Tiago Ferreira <tiagopoleze@icloud.com>
2023-05-03 12:42:18 +0200

    Removed documentation creationg

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
         .package(url: "https://github.com/realm/SwiftLint", branch: "main")
     ],
     targets: [
